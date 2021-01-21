af3cf30862078c719ee07410b83b8bc72202a161
Qi Cao <alanqicao@gmail.com>
2021-01-21 00:32:48 -0700

    graph charts

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import './Newsfeed.css'
+import React from 'react';
+import './Newsfeed.css';
+import LineGraph from './LineGraph'
 
 function Newsfeed() {
   return (
     <div className='newsfeed'>
       <div className="newsfeed__container">
-            
+            <div className='newsfeed__cartSection'>
+              <div className="newsfeed__portfolio">
+                <h1>$114,656</h1>
+                <p>+$42.63 (+0.04%) Today</p>
+
+              </div>
+              <div className="newsfeed__chart">
+                <LineGraph/>
+              </div>
+            </div>
       </div>
       
     </div>
--- src/Stats.js
+++ src/Stats.js
@@ -5,7 +5,24 @@ function Stats() {
   return (
     <div className='stats'>
       <div className="stats__container">
-        
+        <div className="stats__header">
+          <p>Stocks</p>
+        </div>
+        <div className="stats__content">
+          <div className="stats__rows">
+        {/* {for out current stocks} */}
+          </div>
+        </div>
+
+        <div className="stats__header">
+          <p>Lists</p>
+
+        </div>
+        <div className="stats__content">
+          <div className="stats__rows">
+          {/* {stocks we can buy} */}
+          </div>
+        </div>
       </div>
       
     </div>
